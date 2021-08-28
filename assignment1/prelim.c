045e400a19b93bd5f54403a55ffc6decb4022ca5
Kailash Chivhe <kchivhe@uncc.edu>
2021-08-28 19:32:53 -0400

    Added Assignment 1 file and completed code for Question 1

---
+++ assignment1/prelim.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+#include <stdio.h>
+int main () {
+  char hostname[1024];
+  hostname[1023] = '\0';
+  gethostname(hostname, 1023);
+  printf("Hostname: %s\n", hostname);
+  return 0;
+}
