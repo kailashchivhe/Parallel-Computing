854e3c4ac6925983c775e36f0a86ebfbeba63de6
Kailash Chivhe <kchivhe@uncc.edu>
2021-10-03 22:54:09 -0400

    updated test cases file

---
+++ assignment2/dynamic/test.sh
@@ -0,0 +1,110 @@
+##dynamic loop test
+
+TEST9=$(./dynamic_sched 1 0 10 10000 1 1 iteration 100 2> /dev/null)
+if ./approx $TEST9 50;
+then
+    echo oktest9
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 1 iteration 100" should give roughly "50"
+    exit 1
+fi
+
+TEST10=$(./dynamic_sched 1 0 10 10000 1 4 iteration 100 2> /dev/null)
+if ./approx $TEST10 50;
+then
+    echo oktest10
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 4 iteration 100" should give roughly "50"
+    exit 1
+fi
+
+TEST11=$(./dynamic_sched 1 0 10 10000 1 8 iteration 100 2> /dev/null)
+if ./approx $TEST11 50;
+then
+    echo oktest11
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 8 iteration 100" should give roughly "50"
+    exit 1
+fi
+
+TEST12=$(./dynamic_sched 1 0 10 10000 1 16 iteration 100 2> /dev/null)
+if ./approx $TEST12 50;
+then
+    echo oktest12
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 16 iteration 100" should give roughly "50"
+    exit 1
+fi
+
+TEST13=$(./dynamic_sched 1 0 10 10000 1 1 thread 100 2> /dev/null)
+if ./approx $TEST13 50;
+then
+    echo oktest13
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 1 thread 100" should give roughly "50"
+    exit 1
+fi
+
+TEST14=$(./dynamic_sched 1 0 10 10000 1 4 thread 100 2> /dev/null)
+if ./approx $TEST14 50;
+then
+    echo oktest14
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 4 thread 100" should give roughly "50"
+    exit 1
+fi
+
+TEST15=$(./dynamic_sched 1 0 10 10000 1 8 thread 100 2> /dev/null)
+if ./approx $TEST15 50;
+then
+    echo oktest15
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 8 thread 100" should give roughly "50"
+    exit 1
+fi
+
+TEST16=$(./dynamic_sched 1 0 10 10000 1 16 thread 100 2> /dev/null)
+if ./approx $TEST16 50;
+then
+    echo oktest16
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 16 thread 100" should give roughly "50"
+    exit 1
+fi
+
+TEST17=$(./dynamic_sched 1 0 10 10000 1 1 chunk 100 2> /dev/null)
+if ./approx $TEST17 50;
+then
+    echo oktest17
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 1 chunk 100" should give roughly "50"
+    exit 1
+fi
+
+TEST18=$(./dynamic_sched 1 0 10 10000 1 4 chunk 100 2> /dev/null)
+if ./approx $TEST18 50;
+then
+    echo oktest18
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 4 chunk 100" should give roughly "50"
+    exit 1
+fi
+
+TEST19=$(./dynamic_sched 1 0 10 10000 1 8 chunk 100 2> /dev/null)
+if ./approx $TEST19 50;
+then
+    echo oktest19
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 8 chunk 100" should give roughly "50"
+    exit 1
+fi
+
+
+TEST20=$(./dynamic_sched 1 0 10 10000 1 16 chunk 100 2> /dev/null)
+if ./approx $TEST20 50;
+then
+    echo oktest20
+else
+    echo notok "./dynamic_sched 1 0 10 10000 1 16 chunk " should give roughly "50"
+    exit 1
+fi
