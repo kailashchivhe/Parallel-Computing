64dedf5fd47860fbbfba06330a174ea417b83fc4
Kailash Chivhe <kchivhe@gmail.com>
2021-10-10 11:52:43 -0400

    static changes in naming

--- assignment2/static/static_sched.cpp
+++ assignment2/static/static_sched.cpp
@@ -21,7 +21,7 @@ extern "C"
 #endif
 struct static_data
 {
-  float a, b, result, x_val = 0, x_int, t1;
+  float a, b, result, sum = 0, x, t1;
   int intensity, func, n, start, end;
 };
 
@@ -31,36 +31,27 @@ pthread_mutex_t global_sum_lock;
 void *thread_sync(void *argument)
 {
   struct static_data *thread_data = (struct static_data *)argument;
-
   for (int i = thread_data->start; i < thread_data->end; i++)
   {
-    thread_data->x_int = (thread_data->a + ((float)i + 0.5) * thread_data->t1);
+    thread_data->x = (thread_data->a + ((float)i + 0.5) * thread_data->t1);
     switch (thread_data->func)
     {
       case 1:
-      {
-        thread_data->x_val += f1(thread_data->x_int, thread_data->intensity);
+        thread_data->sum += f1(thread_data->x, thread_data->intensity);
         break;
-      }
       case 2:
-      {
-        thread_data->x_val += f2(thread_data->x_int, thread_data->intensity);
+        thread_data->sum += f2(thread_data->x, thread_data->intensity);
         break;
-      }
       case 3:
-      {
-        thread_data->x_val += f3(thread_data->x_int, thread_data->intensity);
+        thread_data->sum += f3(thread_data->x, thread_data->intensity);
         break;
-      }
       case 4:
-      {
-        thread_data->x_val += f4(thread_data->x_int, thread_data->intensity);
+        thread_data->sum += f4(thread_data->x, thread_data->intensity);
         break;
-      }
       default:
         std::cout << "\nWrong function id" << std::endl;
     }
-    thread_data->result = thread_data->x_val * thread_data->t1;
+    thread_data->result = thread_data->sum * thread_data->t1;
   }
   pthread_exit(NULL);
 }
@@ -75,25 +66,17 @@ void *iteration_sync(void *argument)
     switch (thread_data->func)
     {
       case 1:
-      {
         global_x_sum += f1(global_x, thread_data->intensity);
         break;
-      }
       case 2:
-      {
         global_x_sum += f2(global_x, thread_data->intensity);
         break;
-      }
       case 3:
-      {
         global_x_sum += f3(global_x, thread_data->intensity);
         break;
-      }
       case 4:
-      {
         global_x_sum += f4(global_x, thread_data->intensity);
         break;
-      }
       default:
         std::cout << "\nWrong function id" << std::endl;
     }
@@ -106,7 +89,7 @@ void *iteration_sync(void *argument)
 int main(int argc, char *argv[])
 {
 
-  float final_result = 0, a, b;
+  float final_result = 0, a, b, local_t1;
   double cpu_time;
   int func, intensity, nbthreads, n;
   char *sync;
@@ -127,46 +110,46 @@ int main(int argc, char *argv[])
 
   pthread_mutex_init(&global_sum_lock, NULL);
 
+  local_t1 = (b-a)/(float)n;
   std::chrono::time_point<std::chrono::system_clock> clock_start = std::chrono::system_clock::now();
 
   if (strcmp(sync, "thread") == 0)
   {
-    for (int j = 0; j < nbthreads; j++)
-    {
-      thread_data[j].a = a;
-      thread_data[j].b = b;
-      thread_data[j].start = j * (n / nbthreads);
-      thread_data[j].end = thread_data[j].start + (n / nbthreads);
-      thread_data[j].intensity = intensity;
-      thread_data[j].func = func;
-      thread_data[j].n = n;
-      thread_data[j].t1 = (b-a)/(float)n;
-      pthread_create(&threads[j], NULL, thread_sync, (void *)&(thread_data[j]));
-    }
     for (int i = 0; i < nbthreads; i++)
     {
-      pthread_join(threads[i], NULL);
+      thread_data[i].a = a;
+      thread_data[i].b = b;
+      thread_data[i].start = i * (n / nbthreads);
+      thread_data[i].end = thread_data[i].start + (n / nbthreads);
+      thread_data[i].intensity = intensity;
+      thread_data[i].func = func;
+      thread_data[i].n = n;
+      thread_data[i].t1 = local_t1;
+      pthread_create(&threads[i], NULL, thread_sync, (void *)&(thread_data[i]));
     }
-    for (int k = 0; k < nbthreads; k++)
+    for (int j = 0; j < nbthreads; j++)
     {
-      global_sum += thread_data[k].result;
+      pthread_join(threads[j], NULL);
+    }
+    for (int m = 0; m < nbthreads; m++)
+    {
+      global_sum += thread_data[m].result;
     }
   }
   else if(strcmp(sync, "iteration") == 0 )
   {
-    for (int j = 0; j < nbthreads; j++)
+    for (int i = 0; i < nbthreads; i++)
     {
-      thread_data[j].a = a;
-      thread_data[j].b = b;
-      thread_data[j].start = j * (n / nbthreads);
-      thread_data[j].end = thread_data[j].start + (n / nbthreads);
-      thread_data[j].intensity = intensity;
-      thread_data[j].func = func;
-      thread_data[j].n = n;
-      thread_data[j].t1 = (b-a)/(float)n;
-      pthread_create(&threads[j], NULL, iteration_sync, (void *)&(thread_data[j]));
+      thread_data[i].a = a;
+      thread_data[i].b = b;
+      thread_data[i].start = i * (n / nbthreads);
+      thread_data[i].end = thread_data[i].start + (n / nbthreads);
+      thread_data[i].intensity = intensity;
+      thread_data[i].func = func;
+      thread_data[i].n = n;
+      thread_data[i].t1 = local_t1;
+      pthread_create(&threads[i], NULL, iteration_sync, (void *)&(thread_data[i]));
     }
-
     for (int j = 0; j < nbthreads; j++)
     {
       pthread_join(threads[j], NULL);
