54c4d8c916d72075bfbf5a1da8b369aada207c51
Kailash Chivhe <kchivhe@gmail.com>
2021-10-22 15:08:38 -0400

    Added reduce and numint for test

--- assignment7/numint/numint.cpp
+++ assignment7/numint/numint.cpp
@@ -19,11 +19,46 @@ float f4(float x, int intensity);
 }
 #endif
 
+double calc_numerical_integration(int functionid, float a, float b, int n, int intensity)
+{
+    float sum=0;
+    float w = (b-a)/(float)n;
+    float x;
 
+    if ( functionid == 1 ){
+      #pragma omp parallel for 
+          for (int i=0;i<n;i++) {
+            x = a + (((float)i + 0.5) * w);
+            sum= sum + f1(x, intensity);
+          }
+      return (sum*w);
+    }else if ( functionid == 2 ){
+        #pragma omp parallel for 
+          for (int i=0;i<n;i++) {
+            x = a + (((float)i + 0.5) * w);
+            sum= sum + f2(x, intensity);
+          }
+      return (sum*w);
+    }else if ( functionid == 3) {
+        #pragma omp parallel for 
+          for (int i=0;i<n;i++) {
+            x = a + (((float)i + 0.5) * w);
+            sum= sum + f3(x, intensity);
+          }
+        return (sum*w);
+    }else if ( functionid == 4 ) {
+        #pragma omp parallel for 
+          for (int i=0;i<n;i++) {
+            x = a + (((float)i + 0.5) * w);
+            sum= sum + f4(x, intensity);
+          }
+        return (sum*w);
+    }
+}
 
 int main (int argc, char* argv[]) {
   //forces openmp to create the threads beforehand
-#pragma omp parallel
+  #pragma omp parallel
   {
     int fd = open (argv[0], O_RDONLY);
     if (fd != -1) {
@@ -40,6 +75,42 @@ int main (int argc, char* argv[]) {
   }
 
   //insert code here
+
+  float a, b,result;
+   
+  int function_id, intensity, nbthreads,n;
+  function_id = atoi(argv[1]);
+  a = atof(argv[2]);
+  b = atof(argv[3]);
+  n = atoi(argv[4]);
+  intensity = atoi(argv[5]);
+  nbthreads = atoi(argv[6]);
+  string kind = argv[7];  
+    
+  if (kind.compare("static")==0)
+  {
+    #pragma omp parallel
+    #pragma omp omp_set_schedule(static)
+      result=calc_numerical_integration(function_id, a, b, n, intensity);
+  }
+  else if (kind.compare("dynamic")==0){
+    #pragma omp parallel
+    #pragma omp omp_set_schedule(dynamic)
+      result=calc_numerical_integration(function_id, a, b, n, intensity);
+  }
+  else if (kind.compare("guided")==0){
+    #pragma omp parallel
+    #pragma omp omp_set_schedule(guided)
+      result=calc_numerical_integration(function_id, a, b, n, intensity);
+  }
+ 
+  gettimeofday(&end, NULL);
+  
+  cout<<result; 
+  		
+  double time = ((double)end.tv_sec-(double)start.tv_sec+((double)end.tv_usec-(double)start.tv_usec)/(double)1000000); // in seconds
+  
+  std::cerr<<time;
 
   return 0;
 }
--- assignment7/reduce/reduce.cpp
+++ assignment7/reduce/reduce.cpp
@@ -18,7 +18,7 @@ extern "C" {
 
 int main (int argc, char* argv[]) {
     //forces openmp to create the threads beforehand
-#pragma omp parallel
+    #pragma omp parallel
     {
         int fd = open (argv[0], O_RDONLY);
         if (fd != -1) {
@@ -35,13 +35,47 @@ int main (int argc, char* argv[]) {
     }
     
     int n = atoi(argv[1]);
-    int * arr = new int [n];
+    int *arr = new int [n];
+    int nthreads = atoi(argv[2]);
+    string kind = argv[3];
+    int gran = atoi(argv[4]);
+    int result=0;
     
     generateReduceData (arr, atoi(argv[1]));
     
     // insert reduction code here
 
-    
+    if (kind.compare("static")==0)
+    {
+        #pragma omp parallel for ordered schedule(static)
+        for (int i = 0; i < n; i++) {
+            sum=sum+ arr[i];
+        } 
+    }
+    else if (kind.compare("dynamic")==0)
+    {
+        #pragma omp omp_set_schedule(dynamic)
+        #pragma omp parallel for 
+            for (int i = 0; i < n; i++) {
+                sum = sum + arr[i];
+            }
+    }
+    else if (kind.compare("guided")==0)
+    {
+        #pragma omp omp_set_schedule(guided)
+        #pragma omp parallel for 
+            for (int i = 0; i < n; i++) {
+                sum = sum + arr[i];
+            }
+    }
+
+    gettimeofday(&end, NULL);
+    cout<<sum;
+            
+    double time = ((double)end.tv_sec- (double)start.tv_sec+( (double) end.tv_usec- (double)start.tv_usec)/ (double)1000000; // in seconds
+
+    std::cerr<<time;
+
     delete[] arr;
     
     return 0;
