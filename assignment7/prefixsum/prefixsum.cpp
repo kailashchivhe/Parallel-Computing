ea2bdc40d30cf9fccdfd8811fb296ab7e456e535
Kailash Chivhe <kchivhe@gmail.com>
2021-10-27 16:46:20 -0400

    Prefix initial

--- assignment7/prefixsum/prefixsum.cpp
+++ assignment7/prefixsum/prefixsum.cpp
@@ -39,15 +39,61 @@ int main (int argc, char* argv[]) {
   int n = atoi(argv[1]);
 
   int * arr = new int [n];
+
   generatePrefixSumData (arr, n);
 
   int * pr = new int [n+1];
 
   //insert prefix sum code here
-
   
+  omp_set_num_threads(nbthread);
+
+  omp_set_schedule(omp_sched_dynamic,-1);
+  
+  int *tarr = new int[nbthread];
+  
+  pr[0] = 0;
+
+  //insert prefix sum code here
+
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+
+  #pragma omp parallel
+  {
+    int id = omp_get_thread_num();
+    int total = 0;
+
+    #pragma omp for schedule(runtime)
+    for (int i = 0; i < n; i++)
+    {
+      total += arr[i];
+      pr[i + 1] = total;
+    }
+
+    #pragma omp critical
+    tarr[id] = total;
+
+    #pragma omp barrier
+    int sum = 0;
+    for (int j = 0; j < id; j++)
+    {
+      sum += tarr[j];
+    }
+
+    #pragma omp for schedule(runtime)
+    for (int k = 0; k < n; k++)
+    {
+      pr[k + 1] += sum;
+    }
+  }
+
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  
+  std::chrono::duration<double> elapsed_seconds = end-start;
   
   checkPrefixSumResult(pr, n);
+
+  std::cerr<<elapsed_seconds.count()<<std::endl;
 
   delete[] arr;
 
