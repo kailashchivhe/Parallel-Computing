79afe9f57bcae03cdb53e7f5f04df93ec4e1a683
Kailash Chivhe <kchivhe@gmail.com>
2021-10-27 20:41:23 -0400

    Merge sort and Prefix screenshot

--- assignment7/mergesort/mergesort.cpp
+++ assignment7/mergesort/mergesort.cpp
@@ -4,6 +4,10 @@
 #include <fcntl.h>
 #include <iostream>
 #include <unistd.h>
+#include <omp.h>
+#include <chrono>
+
+using namespace std;
 
 #ifdef __cplusplus
 extern "C" {
@@ -14,38 +18,124 @@ extern "C" {
 }
 #endif
 
+void merge( int *arr, int n, int *tmpArr )
+{
+  int i = 0;
+  int j = n / 2;
+  int k = 0;
+  while (i < n / 2 && j < n)
+  {
+    if (arr[i] < arr[j])
+    {
+      tmpArr[k] = arr[i];
+      k++;
+      i++;
+    }
+    else
+    {
+      tmpArr[k] = arr[j];
+      k++;
+      j++;
+    }
+  }
+  #pragma omp task
+  {
+    while (i < n / 2)
+    {
+      tmpArr[k] = arr[i];
+      k++;
+      i++;
+    }
+  }
+  #pragma omp task
+  {
+    while (j < n)
+    {
+      tmpArr[k] = arr[j];
+      k++;
+      j++;
+    }
+  }
 
-int main (int argc, char* argv[]) {
+  #pragma omp taskwait
+  memcpy(arr, tmpArr, n * sizeof(int));
+}
+
+void mergesort(int * arr, int n, int * tmp)
+{
+   if (n < 2) 
+   {
+      return;
+   }
+
+   #pragma omp task firstprivate( X, n, tmp )
+   mergesort( arr, n/2, tmp );
+
+   #pragma omp task firstprivate( X, n, tmp )
+   mergesort( arr+(n/2), n-(n/2), tmp );
+	
+   #pragma omp taskwait
+
+   #pragma omp parallel
+  {
+    #pragma omp single
+    merge( arr, n, tmp );
+  }
+}
+
+int main(int argc, char *argv[])
+{
 
   //forces openmp to create the threads beforehand
 #pragma omp parallel
   {
-    int fd = open (argv[0], O_RDONLY);
-    if (fd != -1) {
-      close (fd);
+    int fd = open(argv[0], O_RDONLY);
+    if (fd != -1)
+    {
+      close(fd);
     }
-    else {
-      std::cerr<<"something is amiss"<<std::endl;
+    else
+    {
+      std::cerr << "something is amiss" << std::endl;
     }
   }
-  
-  if (argc < 3) { std::cerr<<"Usage: "<<argv[0]<<" <n> <nbthreads>"<<std::endl;
+
+  if (argc < 3)
+  {
+    std::cerr << "Usage: " << argv[0] << " <n> <nbthreads>" << std::endl;
     return -1;
   }
 
   int n = atoi(argv[1]);
-  
+  int nbthreads = atoi(argv[2]);
+
+  omp_set_num_threads(nbthread);
+
+  omp_set_schedule( omp_sched_static,-1 );
+
   // get arr data
-  int * arr = new int [n];
-  generateMergeSortData (arr, n);
+  int *arr = new int[n];
+  int *tmp = new int[n];
+
+  generateMergeSortData(arr, n);
 
   //insert sorting code here.
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+  #pragma omp parallel
+  {
+    #pragma omp single
+    mergesort(arr, n, tmp);
+  }
 
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end-start;
 
-  
-  checkMergeSortResult (arr, n);
-  
+  checkMergeSortResult(arr, n);
+
+  std::cerr<<elapsed_seconds.count()<<std::endl;
+
   delete[] arr;
+  delete[] tmp;
 
   return 0;
 }
