6448053f86dab89a876a128b201d4b0cf68b6be8
Kailash Prakash Chivhe <kchivhe@gal-i1.uncc.edu>
2021-11-25 13:48:06 -0500

    MPI hello world

--- assignment10/hello_world/hello.cpp
+++ assignment10/hello_world/hello.cpp
@@ -4,15 +4,10 @@
 
 int main(int argc, char*argv[]) {
     int i, id, p;
-    int solutions, global_solutions;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
-    for (i = id; i < 65535; i += p)
-    solutoins += check_circuit(id, i);
-    MPI_Reduce(&solutions, &global_solutions, 1, MPI_INT, MPI_SUM,
-    0, MPI_COMM_WORLD);
-    printf("Process %d is done\n", id);
+    printf("I am Process %d is out of %d \n", id, p);
     MPI_Finalize();
     return 0;
 }
